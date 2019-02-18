92ee578599a964b356850521f7b0985a755070b1
Unknown <norama.matema@gmail.com>
2019-02-18 14:46:17 +0100

    context version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import actions from './actions';
 import './App.css';
 
 function App({food, searchTerm, searchTermChanged}) {
@@ -37,4 +35,4 @@ function App({food, searchTerm, searchTermChanged}) {
   );
 }
 
-export default connect(store => store, actions)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers from './reducers';
+import Provider from './providers';
+import Consumer from './consumer';
 import App from './App';
 
-// Creating the store using the reducers info.
-// That's because reducers are the building blocks of a Redux Store.
-const store = createStore(reducers);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
+  <Provider>
+    <Consumer>
+      <App />
+    </Consumer>
   </Provider>,
   document.getElementById('root')
 );
